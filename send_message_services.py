5f1803ccaf89eb889c7e18fd0c51af9087bb173a
anilkumarKanasani <49880954+anilkumarKanasani@users.noreply.github.com>
2024-01-20 11:03:51 +0530

    Update send_message_services.py

--- send_message_services.py
+++ send_message_services.py
@@ -7,19 +7,24 @@ env = Env()
 env.read_env("./.env")
 
 
-def send_telegram_message(msg: str):
+def send_telegram_message(msg: str, backup: bool):
     """
     Function to send a Telegram message.
 
     Parameters:
     msg (str): The message to send.
+    backup (bool): redirect to backup chat or not.
 
     Returns:
     None
     """
 
-    TOKEN = env("TELEGRAM_TOKEN")
-    chat_id = env("TELEGRAM_CHAT_ID")
+    if backup:
+        TOKEN = env("BKP_TELEGRAM_TOKEN")
+        chat_id = env("BKP_TELEGRAM_CHAT_ID")
+    else:
+        TOKEN = env("TELEGRAM_TOKEN")
+        chat_id = env("TELEGRAM_CHAT_ID")
 
     url = (
         f"https://api.telegram.org/bot{TOKEN}/sendMessage"
