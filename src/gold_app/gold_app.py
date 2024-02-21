25b5883bdd6135e508763a9473ea024df51644fc
anilkumarKanasani <anilkumar.kanasani@gmail.com>
2024-02-21 06:36:10 +0100

    unsquried request to website

--- src/gold_app/gold_app.py
+++ src/gold_app/gold_app.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 
 def get_gold_price():
     url = "https://www.grtjewels.com/"
-    page = requests.get(url)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    page = requests.get(url, headers=headers, verify=False, timeout=10)
     soup = BeautifulSoup(page.content, "html.parser")
     rate_div = soup.find_all('div', class_='rate slide-rates')
     rate_text = rate_div[0].text
